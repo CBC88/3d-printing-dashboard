a773b9cb6f72f1044ca5d3043bccf076139788e0
CBC <chrisborgc1988@gmail.com>
2025-06-11 01:22:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
+import os
 from map_dashboard import app
 
 if __name__ == "__main__":
-    # Locally this will run on http://localhost:8050
-   # app.run_server(host="0.0.0.0", port=8050, debug=False)
-    app.run(host="0.0.0.0", port=8050, debug=False)
+    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 8050)), debug=False)
